8ac44c31c308d7e6f252c4d07605cc742ff505ff
NoorHajDawood <noorhajdawood@gmail.com>
2021-06-06 16:00:36 +0300

    +comments

--- emailTrash.ino
+++ emailTrash.ino
@@ -1,27 +1,24 @@
+// EmailSender Library and email to be used by the kit
 #include <EMailSender.h>
 EMailSender emailSend("trashArduino@gmail.com", "7Fdf8591");
+EMailSender::EMailMessage message;
 
-//#include <ESP8266WiFi.h>
+// Wifi and Cloud libraries
 #include <WiFi.h>
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 
-
 /********* WiFi Access Point ***********/
-
-#define WLAN_SSID       "Shenkar-New"
-#define WLAN_PASS       "Shenkarwifi"
+#define WLAN_SSID "Shenkar-New"
+#define WLAN_PASS "Shenkarwifi"
 
 /********* Adafruit.io Setup ***********/
-
-#define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  1883                   // use 8883 for SSL
-#define AIO_USERNAME    "NoorX"
-#define AIO_KEY         "aio_KFxV53xCPxN8NZDzx4Bv2yC67Pph"
-
+#define AIO_SERVER "io.adafruit.com"
+#define AIO_SERVERPORT 1883 // use 8883 for SSL
+#define AIO_USERNAME "NoorX"
+#define AIO_KEY "aio_KFxV53xCPxN8NZDzx4Bv2yC67Pph"
 
 /**** Global State (you don't need to change this!) ******/
-
 
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
 WiFiClient client;
@@ -42,13 +39,14 @@ Adafruit_MQTT_Subscribe distance_sub = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNA
 
 /********* Sketch Code ************/
 
+// distance input
 #define trigPin 27
-
 #define echoPin 14
-
+// led output
 #define greenLED 25
-
 #define redLED 26
+
+// counters to prevent excessive email sending
 int countFull = 0;
 int countEmpty = 0;
 
@@ -56,127 +54,129 @@ int countEmpty = 0;
 // for some reason (only affects ESP8266, likely an arduino-builder bug).
 void MQTT_connect();
 
-EMailSender::EMailMessage message;
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   delay(1000);
   Serial.println(F("Starting..."));
   delay(1000);
   Serial.println(F("\n\n##################################"));
-  Serial.println(F("Trash"));
+  Serial.println(F("Biny The Bin"));
   Serial.println(F("Noor & Bader & ADI & NOY"));
   Serial.println(F("##################################"));
-  
+
   // Connect to WiFi access point.
-  Serial.println(); Serial.println();
+  Serial.println();
+  Serial.println();
   Serial.print("Connecting to ");
   Serial.println(WLAN_SSID);
 
   WiFi.begin(WLAN_SSID, WLAN_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   Serial.println();
 
   Serial.println("WiFi connected");
-  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
   Serial.println();
-// SETUP ALL THE SUBSRCIPTIONS HERE 
-  mqtt.subscribe(&distance_sub);     // Setup MQTT subscription for onoff feed.
+  // SETUP ALL THE SUBSRCIPTIONS HERE
+  mqtt.subscribe(&distance_sub); // Setup MQTT subscription for onoff feed.
 
-pinMode(trigPin, OUTPUT);
-
-pinMode(echoPin, INPUT);
-
-pinMode(greenLED, OUTPUT);
-
-pinMode(redLED, OUTPUT);
-  
-
+  // SETUP PINS
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(greenLED, OUTPUT);
+  pinMode(redLED, OUTPUT);
 }
 
-
-
-
-void loop() {
-// We must keep this for now
+void loop()
+{
+  // We must keep this for now
   MQTT_connect();
 
+  // Reading from the cloud subscription
   Serial.println(F("Reading From Cloud:"));
   Adafruit_MQTT_Subscribe *subscription;
-  while ((subscription = mqtt.readSubscription(500))) {
-    if (subscription == &distance_sub) {
+  while ((subscription = mqtt.readSubscription(500)))
+  {
+    if (subscription == &distance_sub)
+    {
       Serial.print(F("Got: "));
       Serial.println((char *)distance_sub.lastread);
     }
   }
 
-  long duration; // distance;
+  // calculating distance input from sensor
+  long duration;
   int distance;
 
-digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); //10
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) / 29.1;
 
-delayMicroseconds(2);
+  // This is where the LED On/Off happens indicating that trash is getting full
+  if (distance < 5)
+  {
 
-digitalWrite(trigPin, HIGH);
+    digitalWrite(greenLED, HIGH);
+    digitalWrite(redLED, LOW);
+    //upload to cloud distance where condition met 
+    Serial.print(distance);
+    Serial.println(" cm");
+    light_level.publish(distance);
+    /*
+    only send email if trash got detected to be full 3 times (after empty status)
+    that way we don't send for false-positive and we prevent email spam
+    */
+    ++countFull;
+    if (countFull == 3)
+    {
+      message.subject = "Trash bin is FULL!";
+      message.message = "Biny the bin is full of trash, please come empty him ^_^";
+      EMailSender::Response resp = emailSend.send("trashArduino@gmail.com", message);
+      Serial.println("Sending status: ");
+      Serial.println(resp.code);
+      Serial.println(resp.desc);
+      Serial.println(resp.status);
+    }
+    delay(5000);
+  }
+  else
+  {
+    digitalWrite(greenLED, LOW);
+    digitalWrite(redLED, HIGH);
+    /*
+    detecting empty status and checking for false-positives
+    */
+    ++countEmpty;
+    if (countEmpty == 2)
+    {
+      countEmpty = 0;
+      countFull = 0;
+    }
+    // upload to cloud
+    light_level.publish(0);
+    delay(5000);
+  }
 
-delayMicroseconds(10); //10
-
-digitalWrite(trigPin, LOW);
-
-duration = pulseIn(echoPin, HIGH);
-
-distance = (duration/2) / 29.1;
-
-if (distance < 5) { // This is where the LED On/Off happens
-
-digitalWrite(greenLED,HIGH);digitalWrite(redLED,LOW); // When the Red condition is met, the Green LED should turn off digitalWrite(redLED,LOW);
-Serial.print(distance); Serial.println(" cm");
-light_level.publish(distance);
-++countFull;
-if(countFull==3)
-{
-  message.subject = "Trash bin is FULL!";
-message.message = "Biny the bin is full of trash, please come empty him ^_^";
-EMailSender::Response resp = emailSend.send("trashArduino@gmail.com", message);
-Serial.println("Sending status: ");
-Serial.println(resp.code);
-Serial.println(resp.desc);
-Serial.println(resp.status);
-}
-delay(5000);
-
-} else {
-
-digitalWrite(greenLED,LOW); digitalWrite(redLED,HIGH);
-++countEmpty;
-if(countEmpty==2)
-{
-  countEmpty=0;
-  countFull=0;
-}
-light_level.publish(0);
-delay(5000);
-}
-
-if (distance >= 200 || distance <= 0){
-
-Serial.println("Out of range");
-
-} else {
-
-Serial.print(distance); Serial.println(" cm");
-
-} 
-//delay(500);
-  // increment the count by 1
-//  if (! light_level.publish(count)) {
-//    Serial.println(F("Failed"));
-//  } else {
-//    Serial.println(F("OK!"));
-//  }
+  // log to serial monitor
+  if (distance >= 200 || distance <= 0) // if sensor reads false values
+  {
+    Serial.println("Out of range");
+  }
+  else
+  {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
 
   // ping the server to keep the mqtt connection alive
   // NOT required if you are publishing once every KEEPALIVE seconds
@@ -185,35 +185,36 @@ Serial.print(distance); Serial.println(" cm");
     mqtt.disconnect();
   }
   */
-
 }
-
-
-
 
 // Function to connect and reconnect as necessary to the MQTT server.
 // Should be called in the loop function and it will take care if connecting.
-void MQTT_connect() {
+void MQTT_connect()
+{
   int8_t ret;
 
   // Stop if already connected.
-  if (mqtt.connected()) {
+  if (mqtt.connected())
+  {
     return;
   }
 
   Serial.print("Connecting to MQTT... ");
 
   uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-       Serial.println(mqtt.connectErrorString(ret));
-       Serial.println("Retrying MQTT connection in 5 seconds...");
-       mqtt.disconnect();
-       delay(5000);  // wait 5 seconds
-       retries--;
-       if (retries == 0) {
-         // basically die and wait for WDT to reset me
-         while (1);
-       }
+  while ((ret = mqtt.connect()) != 0)
+  { // connect will return 0 for connected
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000); // wait 5 seconds
+    retries--;
+    if (retries == 0)
+    {
+      // basically die and wait for WDT to reset me
+      while (1)
+        ;
+    }
   }
   Serial.println("MQTT Connected!");
 }
--- servoTrash.ino
+++ servoTrash.ino
@@ -1,13 +1,14 @@
-#include <ESP32Servo.h>   //servo library
-Servo servo;     
-int trigPin = 27;    
-int echoPin = 14;   
+#include <ESP32Servo.h> //servo library
+Servo servo;
+// i/o pins
+int trigPin = 27;
+int echoPin = 14;
 int servoPin = 25;
 int redLED = 26;
-int led= 10;
-long duration, dist, average;   
-long aver[3];   //array for average
-
+int led = 10;
+// distance vars
+long duration, dist, average;
+long aver[3]; //array for average
 
 //#include <ESP8266WiFi.h>
 #include <WiFi.h>
@@ -16,18 +17,17 @@ long aver[3];   //array for average
 
 /********* WiFi Access Point ***********/
 
-#define WLAN_SSID       "Shenkar-New"
-#define WLAN_PASS       "Shenkarwifi"
+#define WLAN_SSID "Shenkar-New"
+#define WLAN_PASS "Shenkarwifi"
 
 /********* Adafruit.io Setup ***********/
 
-#define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  1883                   // use 8883 for SSL
-#define AIO_USERNAME    "NoorX"
-#define AIO_KEY         "aio_KFxV53xCPxN8NZDzx4Bv2yC67Pph"
+#define AIO_SERVER "io.adafruit.com"
+#define AIO_SERVERPORT 1883 // use 8883 for SSL
+#define AIO_USERNAME "NoorX"
+#define AIO_KEY "aio_KFxV53xCPxN8NZDzx4Bv2yC67Pph"
 
 /**** Global State (you don't need to change this!) ******/
-
 
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
 WiFiClient client;
@@ -50,146 +50,166 @@ Adafruit_MQTT_Subscribe distance_sub = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNA
 // for some reason (only affects ESP8266, likely an arduino-builder bug).
 void MQTT_connect();
 
-
-
 /********* WiFi Access Point ***********/
 
-#define WLAN_SSID       "Shenkar-New"
-#define WLAN_PASS       "Shenkarwifi"
+#define WLAN_SSID "Shenkar-New"
+#define WLAN_PASS "Shenkarwifi"
 
 /********* Adafruit.io Setup ***********/
 
-#define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  1883                   // use 8883 for SSL
-#define AIO_USERNAME    "NoorX"
-#define AIO_KEY         "aio_KFxV53xCPxN8NZDzx4Bv2yC67Pph"
+#define AIO_SERVER "io.adafruit.com"
+#define AIO_SERVERPORT 1883 // use 8883 for SSL
+#define AIO_USERNAME "NoorX"
+#define AIO_KEY "aio_KFxV53xCPxN8NZDzx4Bv2yC67Pph"
 
-void setup() {       
-    Serial.begin(9600);
-    servo.attach(servoPin);  
-    pinMode(trigPin, OUTPUT);  
-    pinMode(echoPin, INPUT);  
-    pinMode(redLED, OUTPUT);
-    servo.write(0);         //close cap on power on
-    delay(100);
-    servo.detach(); 
-    Serial.println(F("Starting..."));
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println(F("Starting..."));
   delay(1000);
   Serial.println(F("\n\n##################################"));
   Serial.println(F("Trash"));
   Serial.println(F("Noor & Bader & ADI & NOY"));
   Serial.println(F("##################################"));
   // Connect to WiFi access point.
-  Serial.println(); Serial.println();
+  Serial.println();
+  Serial.println();
   Serial.print("Connecting to ");
   Serial.println(WLAN_SSID);
 
   WiFi.begin(WLAN_SSID, WLAN_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   Serial.println();
 
   Serial.println("WiFi connected");
-  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
   Serial.println();
-// SETUP ALL THE SUBSRCIPTIONS HERE 
-  mqtt.subscribe(&distance_sub);     // Setup MQTT subscription for onoff feed.
-  
-} 
+  // SETUP ALL THE SUBSRCIPTIONS HERE
+  mqtt.subscribe(&distance_sub); // Setup MQTT subscription for onoff feed.
 
-void measure() {  
- digitalWrite(10,HIGH);
-digitalWrite(trigPin, LOW);
-delayMicroseconds(5);
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(15);
-digitalWrite(trigPin, LOW);
-pinMode(echoPin, INPUT);
-duration = pulseIn(echoPin, HIGH);
-dist = (duration/2) / 29.1;    //obtain distance
+  // setup pins
+  servo.attach(servoPin);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(redLED, OUTPUT);
+  servo.write(0); //close cap on power on
+  delay(100);
+  servo.detach();
 }
-void loop() { 
+
+void measure()  // measure distance detected by sensor
+{
+  digitalWrite(10, HIGH);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(15);
+  digitalWrite(trigPin, LOW);
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+  dist = (duration / 2) / 29.1; //obtain distance
+}
+
+void loop()
+{
   // We must keep this for now
   MQTT_connect();
-  int flag=0;
-  for (int i=0;i<=2;i++) {   //average distance
-    measure();               
-   aver[i]=dist;            
-    delay(10);              //delay between measurements
+
+  int flag = 0;
+
+  // getting average distance from sensor to prevent false-positives
+  for (int i = 0; i <= 2; i++)
+  { //average distance
+    measure();
+    aver[i] = dist;
+    delay(10); //delay between measurements
   }
- dist=(aver[0]+aver[1]+aver[2])/3;    
+  dist = (aver[0] + aver[1] + aver[2]) / 3;
 
-if ( dist<50 ) {
-  digitalWrite(redLED,LOW); // When the Red condition is met, the Green LED should turn off digitalWrite(redLED,LOW);
-Serial.print(dist); Serial.println(" cm");
-int feedDist = dist;
-light_level.publish(feedDist);
-//Change distance as per your need
- servo.attach(servoPin);
-  delay(1);
- servo.write(0);  
- delay(3000);       
- servo.write(150);    
- delay(1000);
- servo.detach();    
- flag=0;  
-}
-else{
-  flag++;
-}
-if(flag==1){
-  light_level.publish(0);
-  delay(2000);
-}
-Serial.print(dist);
+  // condition to open trash's cover
+  if (dist < 50)
+  {
+    digitalWrite(redLED, LOW); // When the Red condition is met, the Green LED should turn off digitalWrite(redLED,LOW);
+    Serial.print(dist);
+    Serial.println(" cm");
+    int feedDist = dist;
+    // upload to cloud that cover got opened
+    light_level.publish(feedDist);
+    //Change distance as per your need
+    servo.attach(servoPin);
+    delay(1);
+    servo.write(0);
+    delay(3000);
+    servo.write(150);
+    delay(1000);
+    servo.detach();
+    flag = 0;
+  }
+  else
+  {
+    flag++;
+  }
+  if (flag == 1)  // publish only once for closed cover while it didn't open
+  {
+    light_level.publish(0);
+    delay(2000);
+  }
 
+  // log to serial monitor
+  if (dist >= 200 || dist <= 0)
+  {
+    Serial.println("Out of range");
+  }
+  else
+  {
+    Serial.print(dist);
+    Serial.println(" cm");
+  }
 
-if (dist >= 200 || dist <= 0){
-
-Serial.println("Out of range");
-
-} else {
-
-Serial.print(dist); Serial.println(" cm");
-
-} 
-
-////////////////
-
-
+  // reading from cloud's subscription
   Serial.println(F("Reading From Cloud:"));
   Adafruit_MQTT_Subscribe *subscription;
-  while ((subscription = mqtt.readSubscription(500))) {
-    if (subscription == &distance_sub) {
+  while ((subscription = mqtt.readSubscription(500)))
+  {
+    if (subscription == &distance_sub)
+    {
       Serial.print(F("Got: "));
       Serial.println((char *)distance_sub.lastread);
     }
   }
 }
 
-void MQTT_connect() {
+void MQTT_connect()
+{
   int8_t ret;
 
   // Stop if already connected.
-  if (mqtt.connected()) {
+  if (mqtt.connected())
+  {
     return;
   }
 
   Serial.print("Connecting to MQTT... ");
 
   uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-       Serial.println(mqtt.connectErrorString(ret));
-       Serial.println("Retrying MQTT connection in 5 seconds...");
-       mqtt.disconnect();
-       delay(5000);  // wait 5 seconds
-       retries--;
-       if (retries == 0) {
-         // basically die and wait for WDT to reset me
-         while (1);
-       }
+  while ((ret = mqtt.connect()) != 0)
+  { // connect will return 0 for connected
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000); // wait 5 seconds
+    retries--;
+    if (retries == 0)
+    {
+      // basically die and wait for WDT to reset me
+      while (1)
+        ;
+    }
   }
   Serial.println("MQTT Connected!");
 }
